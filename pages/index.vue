9e72c0f8082d13707d6fb2dc4919e7aca2c8314a
ECrownofFire <ECrownofFire@gmail.com>
2021-07-05 14:35:40 -0400

    Update iframe height

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
         src="https://qaapp.gaidge.com"
         scrolling="no"
         sandbox="allow-forms allow-same-origin allow-scripts allow-storage-access-by-user-activation"
+        height="900"
     />
 </template>
 
@@ -15,9 +16,9 @@ export default Vue.extend({
     computed: {},
 });
 </script>
+
 <style>
 iframe {
     width: 100%;
-    height: 100%;
 }
 </style>
