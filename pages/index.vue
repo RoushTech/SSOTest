249fc27edfa6d2fe9730bfc47ec4da024a6fd441
ECrownofFire <ECrownofFire@gmail.com>
2021-07-05 15:28:05 -0400

    Add frame toggle button

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,24 @@
 <template>
-    <iframe
-        ref="frame"
-        src="https://qaapp.gaidge.com"
-        sandbox="allow-forms allow-same-origin allow-scripts allow-storage-access-by-user-activation"
-        height="900"
-    />
+    <div>
+        <button @click="frame = !frame">Toggle Frame</button>
+        <div v-if="!frame">No frame!</div>
+        <iframe
+            v-else
+            ref="frame"
+            src="https://qaapp.gaidge.com"
+            sandbox="allow-forms allow-same-origin allow-scripts allow-storage-access-by-user-activation"
+            height="900"
+        />
+    </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 
 export default Vue.extend({
-    data: () => {},
+    data: () => ({
+        frame: false,
+    }),
     computed: {},
 });
 </script>
